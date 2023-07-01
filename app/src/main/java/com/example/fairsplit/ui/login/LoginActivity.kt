7d37451e51628b0a78435dfeb9bd060a7f2f4905
abhaysaini <Abhay.saini@appyhigh.com>
2023-07-01 15:57:10 +0530

    Initilaize Application

---
+++ app/src/main/java/com/example/fairsplit/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fairsplit.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fairsplit.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}

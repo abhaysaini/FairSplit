eb4f73ae85f79d2f162ed2d3346e9e8db2e42c14
abhaysaini <Abhay.saini@appyhigh.com>
2023-07-01 23:51:45 +0530

    Google Authentication completed

--- app/src/main/java/com/example/fairsplit/ui/MainActivity.kt
+++ app/src/main/java/com/example/fairsplit/ui/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.example.fairsplit.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.fairsplit.R
+import com.example.fairsplit.databinding.ActivityMainBinding
+import com.example.fairsplit.ui.login.LoginActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var firebaseAuth: FirebaseAuth
+    lateinit var googleClient: GoogleSignInClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("77240101508-ovvgrmu6h93bujqies02s0lur7rl9210.apps.googleusercontent.com")
+            .requestEmail().build()
+        googleClient = GoogleSignIn.getClient(this, gso)
+        binding.logoutButton.setOnClickListener {
+            firebaseAuth.signOut()
+            googleClient.signOut().addOnCompleteListener {
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
+            }
+        }
+
+        binding.tvEmail.text = intent.getStringExtra("email_id")
+        binding.tvName.text = intent.getStringExtra("name")
     }
 }
--- app/src/main/java/com/example/fairsplit/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/fairsplit/ui/login/LoginActivity.kt
@@ -1,12 +1,83 @@
 package com.example.fairsplit.ui.login
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.fairsplit.R
+import com.example.fairsplit.databinding.ActivityLoginBinding
+import com.example.fairsplit.ui.MainActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    lateinit var googleClient: GoogleSignInClient
+    private val launcher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                handleResult(task)
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("77240101508-ovvgrmu6h93bujqies02s0lur7rl9210.apps.googleusercontent.com")
+            .requestEmail().build()
+        googleClient = GoogleSignIn.getClient(this, gso)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.btnLoginWithGoogle.setOnClickListener {
+            signInGoogle()
+        }
+    }
+
+    private fun signInGoogle() {
+        val signInIntent = googleClient.signInIntent
+        launcher.launch(signInIntent)
+    }
+
+    private fun handleResult(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful) {
+            if (task.isSuccessful) {
+                val account: GoogleSignInAccount? = task.result
+                if (account != null) {
+                    updateUI(account)
+                }
+            } else {
+                Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("email_id", account.email)
+                intent.putExtra("name", account.displayName)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }
